fc55a4a73f9ed85fd8e2044ca0d0ed228cf3bf32
Arnaud Sibade <sibade.arnaud@gmail.com>
2018-10-23 16:30:32 +0200

    ajout du projet sur git

---
+++ src/main/java/My_GroupId/My_ArtefactId/App.java
@@ -0,0 +1,13 @@
+package My_GroupId.My_ArtefactId;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
